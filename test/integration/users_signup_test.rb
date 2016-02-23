ec4ceed5c2ad8164df8c18fdc3d2fb1ed20d5196
codeamac10 <codeamac10@gmail.com>
2016-02-22 18:25:37 -0600

    Finish log in & log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,8 +19,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name:  "Example User", email: "user@example.com", password:              "password", password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert is_logged_in?
   end
 
 end
