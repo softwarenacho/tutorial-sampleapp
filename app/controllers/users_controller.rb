820df9b273fac61958e33203e09e6dca411b67c1
codeamac10 <codeamac10@gmail.com>
2016-02-23 12:22:43 -0600

    Changes in update db petitions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,11 @@ class UsersController < ApplicationController
   
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
   end
 
   def new
