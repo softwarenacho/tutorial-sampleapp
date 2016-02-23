337ddc5f23787bee8f26a676e727b49baf8ae929
codeamac10 <codeamac10@gmail.com>
2016-02-23 13:53:55 -0600

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save { email.downcase! }
@@ -67,6 +68,12 @@ class User < ActiveRecord::Base
   
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
